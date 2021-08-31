3852f774b014e9ae5e7746c3e37844e786494169
LiciuStefan <89767945+LiciuStefan@users.noreply.github.com>
2021-08-31 10:06:06 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,17 @@
+document.getElementById.src //take the id element in the html
+.innerHTML// change the content of the text in the id
+window.alert
+console.log
+document.write
+
+
+
+let a,b,c
+a=5;
+b=1;
+c=document.getElementById();
+
+
+
+var a;
+var a="something";
